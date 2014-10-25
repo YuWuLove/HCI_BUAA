969e3570a8b1d629bb2c9587edc0a31f4ac08ae3
YuWuLove <wumark@126.com>
2014-10-25 20:43:44 +0800

    Index Draft
    I design a index page based on a website. And we can start next week.

---
+++ HCI/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HCI::Application.config.secret_token = 'c8d7fb80ede5f8e31972dfd4eb66d53338bcd5835d8149cbfa3d32b35a52c46c24f744884eb257a755354fc0b2da46a4eb7cfb8299d58be2922a53c5e48f8bfd'
