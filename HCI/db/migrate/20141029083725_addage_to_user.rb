879cca453a2c3fabd1adb13bf2550194d1642c56
cdoublej <446297930@qq.com>
2014-10-31 11:19:42 +0800

    database

---
+++ HCI/db/migrate/20141029083725_addage_to_user.rb
@@ -0,0 +1,7 @@
+class AddageToUser < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- HCI/db/schema.rb
+++ HCI/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141025112503) do
+ActiveRecord::Schema.define(:version => 20141029083801) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "pwd"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "age"
   end
 
 end
