d4c28d4bfd0b91d82d4c45a3d7189d7423c037c1
YuWuLove <wumark@126.com>
2014-12-10 20:34:31 +0800

    fix bug

--- HCI/app/models/user_profile.rb
+++ HCI/app/models/user_profile.rb
@@ -1,7 +1,7 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
   attr_accessible :age, :degree, :uid, :avatar , :occupation, :majoy , :location, :user_id
-  has_attached_file :avatar, #:styles => { :medium => "300x300>", :thumb => "100x100>" },
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
   :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type =>  /\Aimage\/.*\Z/
 end
